a7605cba351101e05ff08a4af71552e0e877c39a
Flonx <1244271534flonx@gmail.com>
2020-12-28 20:01:56 +0800

    TestIdea

--- src/com/Flonx/Demo.java
+++ src/com/Flonx/Demo.java
@@ -1,21 +1,86 @@
 package com.Flonx;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Demo
 {
-    public static void main(String[] args)
-    {
+    private static final  int AGE = 10;
 
-        System.out.println("hi,idea");
-        new Date();
-        new HashMap();
-        new ArrayList();
-        System.out.println(Calendar.ALL_STYLES);
-        System.out.println(Arrays.toString(new int[]{1,2,3}));
+    public static void main(String[] args) {
+        int a = 10;
+        for (int i = 0; i < a; i++) {
+
+        }
+        int[] arr = new int[]{1,2,3,4};
+        for(int j = 0;j<arr.length;j++)
+        {
+
+        }
+        System.out.println("Hello World");
+        int age = 18;
+        System.out.println(age);
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Demo.main");
+        System.out.println("age = " + age);
+
+        int b = 10;
+        int c = 20;
+        System.out.println("c = " + c);
+        System.out.println("b = " + b);
 
 
     }
+    public void a(){
+        for (int j = 0; j < 100; j++) {
+
+        }
+        int[] arr = {12,34,56,6,7};
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+        }
+        for (int i : arr) {
+
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        for (String s : list) {
+
+        }
+        for (int i : arr) {
+
+        }
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+
+    }
+    public void b(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        if (list == null) {
+
+        }
+        if (list == null) {
+
+        }
+        if (list != null) {
+
+        }
+        if (list != null) {
+
+        }
+
+    }
+
 
 
 
