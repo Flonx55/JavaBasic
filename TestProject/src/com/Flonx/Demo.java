6c58c2a5487eea4635eff837a2a0241c922738e9
Flonx <1244271534flonx@gmail.com>
2020-12-28 19:09:27 +0800

    TestIdea

---
+++ TestProject/src/com/Flonx/Demo.java
@@ -0,0 +1,23 @@
+package com.Flonx;
+
+import java.util.*;
+
+public class Demo
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("hi,idea");
+        new Date();
+        new HashMap();
+        new ArrayList();
+        System.out.println(Calendar.ALL_STYLES);
+        System.out.println(Arrays.toString(new int[]{1,2,3}));
+
+
+    }
+
+
+
+
+}
