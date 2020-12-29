e161fbfa9811093c3cbded0d09b73d82a9ce851a
Flonx <1244271534flonx@gmail.com>
2020-12-29 18:39:33 +0800

    TestIdea

---
+++ src/com/Flonx2/Test.java
@@ -0,0 +1,29 @@
+package com.Flonx2;
+
+import java.util.Arrays;
+
+public  class  Test{
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int[] arr = new int[4];
+        arr[0] = 11;
+        arr[1] = 56;
+        arr[2] = 98;
+        arr[3] = 66;
+        Arrays.toString(arr);
+        int d = eat();
+        System.out.println(d);
+    }
+    public static int eat(){
+        System.out.println("----1");
+        System.out.println("----2");
+        System.out.println("----3");
+        System.out.println("----4");
+        return 10;
+    }
+}
